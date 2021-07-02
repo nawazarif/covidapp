bf13bd837dfc6d462e944c87faf9f5edf21b470a
nawazarif <76113456+nawazarif@users.noreply.github.com>
2021-07-02 19:33:59 +0500

    Add files via upload
    appdone

---
+++ public/precache-manifest.b9a9e359d6d42e56e7eb66d73839e208.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa9b97bf5730d5c7ccc0f4e06c1f530",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aa29c4c180a1ae68056",
+    "url": "/static/css/main.325928b1.chunk.css"
+  },
+  {
+    "revision": "589fd0ca8bcb6bbbc521",
+    "url": "/static/js/2.7b3209a9.chunk.js"
+  },
+  {
+    "revision": "4ed2d4f00cf8324f872f01907affe619",
+    "url": "/static/js/2.7b3209a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa29c4c180a1ae68056",
+    "url": "/static/js/main.aba51e18.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
